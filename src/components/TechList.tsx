e0fdf24a1057552d367ab15aa4e4c7bc7455074e
valerio <v.scisci@t33.it>
2020-12-29 19:45:24 +0100

    Added a prop to the techList to set the current selected tech on the parent

--- src/components/TechList.tsx
+++ src/components/TechList.tsx
@@ -1,67 +1,73 @@
 import { observer } from 'mobx-react';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaArrowDown, FaArrowUp } from 'react-icons/fa';
 import styled, { css } from 'styled-components';
 import { useStores } from '../hooks/useStores';
 import { TechListItem } from './TechListItem';
 
-export interface TechListProps {}
+export interface TechListProps {
+  onTechChange: (newTech: string) => void;
+}
 
-export const TechList: React.FC<TechListProps> = observer(({}) => {
-  const { home } = useStores();
-  const [currentFirstElement, setCurrentFirstElement] = useState<number>(0);
+export const TechList: React.FC<TechListProps> = observer(
+  ({ onTechChange }) => {
+    const { home } = useStores();
+    const [currentFirstElement, setCurrentFirstElement] = useState<number>(0);
 
-  const handleTechChange = (direction: 'up' | 'down') => {
-    if (direction === 'up') {
-      setCurrentFirstElement(
-        (currentFirstElement % home.techMenuImages.length) - 1,
-      );
-    } else {
-      setCurrentFirstElement(
-        (currentFirstElement % home.techMenuImages.length) + 1,
-      );
-    }
-  };
+    useEffect(() => {
+      onTechChange(home.techMenuImages[currentFirstElement + 2].techName);
+    }, [onTechChange, currentFirstElement, home]);
 
-  return (
-    <Container>
-      <TechMenu>
-        {home.techMenuImages.map((techImage, i) => {
-          if (i >= currentFirstElement && i < currentFirstElement + 5) {
-            return (
-              <TechListItem
-                key={i}
-                image={techImage.img.default}
-                imageAlt={techImage.alt}
-                animationState={i - currentFirstElement}
-              />
-            );
-          }
-        })}
-      </TechMenu>
-      <ControlsContainer>
-        <ControlButton
-          buttonType={'up'}
-          disabled={currentFirstElement < -1}
-          onClick={() => {
-            handleTechChange('up');
-          }}
-        >
-          <FaArrowUp size={'2em'} />
-        </ControlButton>
-        <ControlButton
-          buttonType={'down'}
-          disabled={currentFirstElement > home.techMenuImages.length - 4}
-          onClick={() => {
-            handleTechChange('down');
-          }}
-        >
-          <FaArrowDown size={'2em'} />
-        </ControlButton>
-      </ControlsContainer>
-    </Container>
-  );
-});
+    const handleTechChange = (direction: 'up' | 'down') => {
+      const imageIndex = currentFirstElement % home.techMenuImages.length;
+
+      if (direction === 'up') {
+        setCurrentFirstElement(imageIndex - 1);
+      } else {
+        setCurrentFirstElement(imageIndex + 1);
+      }
+    };
+
+    return (
+      <Container>
+        <TechMenu>
+          {home.techMenuImages.map((techImage, i) => {
+            if (i >= currentFirstElement && i < currentFirstElement + 5) {
+              return (
+                <TechListItem
+                  key={i}
+                  image={techImage.img.default}
+                  imageAlt={techImage.alt}
+                  animationState={i - currentFirstElement}
+                />
+              );
+            }
+          })}
+        </TechMenu>
+        <ControlsContainer>
+          <ControlButton
+            buttonType={'up'}
+            disabled={currentFirstElement < -1}
+            onClick={() => {
+              handleTechChange('up');
+            }}
+          >
+            <FaArrowUp size={'2em'} />
+          </ControlButton>
+          <ControlButton
+            buttonType={'down'}
+            disabled={currentFirstElement > home.techMenuImages.length - 4}
+            onClick={() => {
+              handleTechChange('down');
+            }}
+          >
+            <FaArrowDown size={'2em'} />
+          </ControlButton>
+        </ControlsContainer>
+      </Container>
+    );
+  },
+);
 
 const Container = styled.div`
   height: 100%;
