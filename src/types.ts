6ec30e77791b0e33395816224e5ee34053556b44
valerio <v.scisci@t33.it>
2020-12-29 22:55:35 +0100

    Added a draft of the KnownTechSection

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,16 @@ import ValerioStore from './stores/ValerioStore';
 export interface Stores {
   home: ValerioStore;
 }
+
+export interface ImageSource {
+  alt: string;
+  img: { default: string };
+}
+
+export interface knownTech extends ImageSource {
+  techName: string;
+}
+
+export interface portfolioProject extends ImageSource {
+  madeWith: string[];
+}
