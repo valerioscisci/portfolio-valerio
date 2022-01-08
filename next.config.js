2a937e29330a176014a9333cf9bf42dc7bc3cb81
Valerio Scisci <valerio@themostaza.com>
2022-01-08 11:20:22 +0100

    Added netlify config to read md files

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@ const { i18n } = require('./next-i18next.config');
 module.exports = {
   i18n,
   target: 'serverless',
+  images: {
+    domains: ['localhost', 'www.thewandererdeveloper.com'],
+  },
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader',
+    });
+    return config;
+  },
 };
